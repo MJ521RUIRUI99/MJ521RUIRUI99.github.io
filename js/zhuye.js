397dd7d102118c2f5be24b7bd9fd1aebbcc95ff5
dingxiang <1947298056@qq.com>
2016-11-11 15:07:55 +0800

    蘑菇街

---
+++ js/zhuye.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+    var mybanner = new Swiper ('.zhuye-banner', {
+        loop: true,
+        autoplay: 3000,
+        autoplayDisableOnInteraction: false,
+        paginationClickable :true,
+
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+
+          });
+
+
+};
